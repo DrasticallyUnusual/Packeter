6df388a1a86b4cb2ea4a3eb3bbdac02da66144d1
AO <ouassar@yahoo.fr>
2026-07-28 19:12:16 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Tool: Packeter v1
+# Author: A.O
+# Date: 25 July 2026
+# License: MIT
+"""Packeter - Offline package downloader. Double-click to run."""
+
+from packeter.gui import run
+
+if __name__ == "__main__":
+    run()
